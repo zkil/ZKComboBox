58737c6e7f68f0c140709dad73eff860f33b68f1
zkil <97100170@qq.com>
2017-08-21 16:23:20 +0800

    masonry版本

--- ZKComboBox/ViewController.m
+++ ZKComboBox/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 #import "ZKComboBoxView.h"
+
+#import <Masonry/Masonry.h>
 @interface ViewController ()
 
 @end
@@ -18,8 +20,14 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     ZKComboBoxView *comboxView = [[ZKComboBoxView alloc]initWithFrame:CGRectMake(0, 100, 200, 30)];
-    comboxView.comboBoxDatasource = @[@"11",@"222",@"333",@"444"];
+    comboxView.data = @[@"11",@"222",@"333",@"444"];
+    comboxView.tableHeight = ZKComboBoxViewAutoHeight;
     [self.view addSubview:comboxView];
+    [comboxView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.and.right.mas_equalTo(0);
+        make.height.mas_equalTo(40);
+        make.centerY.mas_equalTo(self.view.mas_centerY);
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
